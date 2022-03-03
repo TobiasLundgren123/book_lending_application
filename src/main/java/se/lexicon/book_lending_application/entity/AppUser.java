965e24d8fd50218dbdd90690a88714a049ca4ab1
TobiasLundgren123 <tobias_lundgren@yahoo.com>
2022-03-03 15:16:52 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/book_lending_application/entity/AppUser.java
@@ -0,0 +1,92 @@
+package se.lexicon.book_lending_application.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int appUserId;
+    private String username;
+    private String password;
+    private LocalDate regDate;
+    private Details userDetails;
+
+    public AppUser(int appUserId, String username, String password, LocalDate regDate, Details userDetails) {
+        this.appUserId = appUserId;
+        this.username = username;
+        this.password = password;
+        this.regDate = regDate;
+        this.userDetails = userDetails;
+    }
+
+    public int getAppUserId() {
+        return appUserId;
+    }
+
+    public void setAppUserId(int appUserId) {
+        this.appUserId = appUserId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(LocalDate regDate) {
+        this.regDate = regDate;
+    }
+
+    public Details getUserDetails() {
+        return userDetails;
+    }
+
+    public void setUserDetails(Details userDetails) {
+        this.userDetails = userDetails;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AppUser appUser = (AppUser) o;
+        return appUserId == appUser.appUserId && username.equals(appUser.username) && password.equals(appUser.password) && Objects.equals(regDate, appUser.regDate) && Objects.equals(userDetails, appUser.userDetails);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(appUserId, username, password, regDate, userDetails);
+    }
+
+    @Override
+    public String toString() {
+        return "AppUser{" +
+                "appUserId=" + appUserId +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", regDate=" + regDate +
+                ", userDetails=" + userDetails +
+                '}';
+    }
+}
